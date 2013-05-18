039cc80b20eb8f8ac31cfda924bf0ee2e184f99c
Hike Danakian <hike@hdanak.com>
2013-05-17 21:38:41 -0700

    Cleaned out stock CRUD actions for Event.

--- app/controllers/events_controller.js
+++ app/controllers/events_controller.js
@@ -1,126 +1,70 @@
 load('application');
+layout(false);
 
-before(loadEvent, {
-    only: ['show', 'edit', 'update', 'destroy']
-    });
 
 action('map', function () {
-/*    render({ page: view, makeEndpoint: makeURL, personaSSO: personaSSO }); */
-    layout(false);
-    render();
+    render({ dummy: new Event() });
+});
+action('list', function () {
+    render({ dummy: new Event() });
 });
 
+function guessFmt() {
+    // TODO: check Content-Type header
+    return params.format
+}
+
+// CREATE
 action(function create() {
-    Event.create(req.body.Event, function (err, event) {
-        respondTo(function (format) {
-            format.json(function () {
+    Event.create(req.body, function (err, event) {
+        switch (guessFmt()) {
+            case "json":
                 if (err) {
                     send({code: 500, error: event && event.errors || err});
                 } else {
                     send({code: 200, data: event.toObject()});
                 }
-            });
-            format.html(function () {
+                break;
+            default:
                 if (err) {
-                    flash('error', 'Event can not be created');
-                    render('new', {
-                        event: event,
-                        title: 'New event'
-                    });
+                    flash('error', 'Event could not be created');
+                    render('new', { event: event,
+                                    title: 'New event' });
                 } else {
                     flash('info', 'Event created');
                     redirect(path_to.events);
                 }
-            });
-        });
-    });
-});
-
-action(function index() {
-    this.title = 'Events index';
-    Event.all(function (err, events) {
-        switch (params.format) {
-            case "json":
-                send({code: 200, data: events});
-                break;
-            default:
-                render({
-                    events: events
-                });
         }
     });
 });
 
+// LIST/FILTER
+action(function index() {
+    Event.all(function (err, events) {
+        switch (guessFmt()) {
+            case "json":
+                send({code: 200, data: events});
+                break;
+            default:
+                render('map', { title: 'Events Listing',
+                                events: events });
+        }
+    });
+});
+
+// DETAILS
 action(function show() {
-    this.title = 'Event show';
-    switch(params.format) {
+    console.log(params.format);
+    switch (guessFmt()) {
         case "json":
             send({code: 200, data: this.event});
             break;
         default:
-            render();
+            render({ title: 'Event Details' });
     }
 });
 
-action(function edit() {
-    this.title = 'Event edit';
-    switch(params.format) {
-        case "json":
-            send(this.event);
-            break;
-        default:
-            render();
-    }
-});
-
-action(function update() {
-    var event = this.event;
-    this.title = 'Edit event details';
-    this.event.updateAttributes(body.Event, function (err) {
-        respondTo(function (format) {
-            format.json(function () {
-                if (err) {
-                    send({code: 500, error: event && event.errors || err});
-                } else {
-                    send({code: 200, data: event});
-                }
-            });
-            format.html(function () {
-                if (!err) {
-                    flash('info', 'Event updated');
-                    redirect(path_to.event(event));
-                } else {
-                    flash('error', 'Event can not be updated');
-                    render('edit');
-                }
-            });
-        });
-    });
-});
-
-action(function destroy() {
-    this.event.destroy(function (error) {
-        respondTo(function (format) {
-            format.json(function () {
-                if (error) {
-                    send({code: 500, error: error});
-                } else {
-                    send({code: 200});
-                }
-            });
-            format.html(function () {
-                if (error) {
-                    flash('error', 'Can not destroy event');
-                } else {
-                    flash('info', 'Event successfully removed');
-                }
-                send("'" + path_to.events + "'");
-            });
-        });
-    });
-});
-
-function loadEvent() {
+before(function loadEvent() {
     Event.find(params.id, function (err, event) {
         if (err || !event) {
             if (!err && !event && params.format === 'json') {
@@ -132,4 +76,4 @@ function loadEvent() {
             next();
         }
     }.bind(this));
-}
+}, { only: ['show'] });
