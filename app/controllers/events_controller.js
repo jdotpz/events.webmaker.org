398f4e19af5196cc64a5f6f5ff388032d745cbcd
Hike Danakian <hike@hdanak.com>
2013-05-20 15:34:16 -0700

    Cleaned up content-negotiation code in Events CRUD controllers.

--- app/controllers/events_controller.js
+++ app/controllers/events_controller.js
@@ -1,6 +1,13 @@
 load('application');
 layout(false);
 
+function resFmt(fmts) {
+    // Optional Content-Type override via 'format' query-parameter.
+    var fmt = req.param('format');
+    if (fmt && fmts[fmt])
+        return fmts[fmt]();
+    return res.format(fmts);
+}
 
 action('map', function () {
     render({ dummy: new Event() });
@@ -9,23 +16,18 @@ action('list', function () {
     render({ dummy: new Event() });
 });
 
-function guessFmt() {
-    // TODO: check Content-Type header
-    return params.format
-}
-
 // CREATE
 action(function create() {
     Event.create(req.body, function (err, event) {
-        switch (guessFmt()) {
-            case "json":
+        resFmt({
+            json: function () {
                 if (err) {
                     send({code: 500, error: event && event.errors || err});
                 } else {
                     send({code: 200, data: event.toObject()});
                 }
-                break;
-            default:
+            },
+            html: function () {
                 if (err) {
                     flash('error', 'Event could not be created');
                     render('new', { event: event,
@@ -34,46 +36,40 @@ action(function create() {
                     flash('info', 'Event created');
                     redirect(path_to.events);
                 }
-        }
-    });
+            }
+        })
+    })
 });
 
 // LIST/FILTER
 action(function index() {
     Event.all(function (err, events) {
-        switch (guessFmt()) {
-            case "json":
-                send({code: 200, data: events});
-                break;
-            default:
+        resFmt({
+            json: function () {
+                send({ code: 200, data: events });
+            },
+            html: function () {
                 render('map', { title: 'Events Listing',
                                 events: events });
-        }
+            }
+        })
     });
 });
 
 // DETAILS
 action(function show() {
-    console.log(params.format);
-    switch (guessFmt()) {
-        case "json":
-            send({code: 200, data: this.event});
-            break;
-        default:
-            render({ title: 'Event Details' });
-    }
-});
-
-before(function loadEvent() {
     Event.find(params.id, function (err, event) {
-        if (err || !event) {
-            if (!err && !event && params.format === 'json') {
-                return send({code: 404, error: 'Not found'});
+        resFmt({
+            json: function () {
+                if (err || !event)
+                    return send({code: 404, error: 'Not found'});
+                send({ code: 200, data: event });
+            },
+            html: function () {
+                if (err || !event)
+                    redirect(path_to.events);
+                render({ title: 'Event Details' });
             }
-            redirect(path_to.events);
-        } else {
-            this.event = event;
-            next();
-        }
-    }.bind(this));
-}, { only: ['show'] });
+        })
+    });
+});
