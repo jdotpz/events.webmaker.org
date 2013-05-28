152c1ab3634f8e189f7b1733b73e08bb712b582b
Hike Danakian <hike@hdanak.com>
2013-05-28 09:30:16 -0700

    Added redis session support for Heroku.

--- config/environment.js
+++ config/environment.js
@@ -3,9 +3,18 @@ module.exports = function (compound) {
     var express = require('express');
     var app = compound.app;
 
-    require("./mozilla")(app);
+    var RedisStore = require('connect-redis')(express),
+        redisOpts = {};
+    if (process.env['REDISTOGO_URL']) {
+        var url = require('url').parse(process.env['REDISTOGO_URL']);
+        redisOpts = {
+            port: url.port,
+            host: url.hostname,
+            pass: url.auth.split(':')[1]
+        };
+    }
 
-    /* Application middleware configuration */
+    require("./mozilla")(app);
 
     app.configure(function(){
         app.use(express.static(app.root + '/public', { maxAge: 86400000 }));
@@ -13,13 +22,17 @@ module.exports = function (compound) {
             src:        'app/assets',
             buildDir:   'public/css',
         }));
-        app.use(express.methodOverride());
         app.use(express.bodyParser());
         app.use(express.cookieParser());
-        app.use(express.cookieSession({
+        app.use(process.env['REDISTOGO_URL'] ? express.session({
             key: "webmaker.sid",
-            secret: app.get('SESSION_SECRET')
+            secret: process.env['SESSION_SECRET'],
+            store: new RedisStore(redisOpts),
+        }) : express.cookieSession({
+            key: "webmaker.sid",
+            secret: process.env['SESSION_SECRET'],
         }));
+        app.use(express.methodOverride());
         app.use(express.csrf());
         app.use(app.router);
     });
